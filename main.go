ea15f803067d97a5005e5a6e9b0222f736701856
ali <alisoltanics@gmail.com>
2019-05-17 13:02:40 +0430

    Connect app to sqlite database

--- main.go
+++ main.go
@@ -1,96 +1,126 @@
-// https://www.youtube.com/watch?v=SqrbIlUwR0U
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"strconv"
-	"math/rand"
-	"github.com/gorilla/mux"
+  "fmt"
+  "encoding/json"
+  "log"
+  "net/http"
+  "github.com/gorilla/mux"
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+  gorm.Model
+  Isbn   string  json:"isbn"
+  Title  string  json:"title"
 }
 
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
 
-var books []Book
+var db *gorm.DB
+var err error
+
+func initialMigration() {
+  db, err = gorm.Open("sqlite3", "test_database.db")
+  if err != nil {
+    fmt.Println(err.Error())
+    panic("Failed to connect to database")
+  }
+  defer db.Close()
+
+  db.AutoMigrate(&Book{})
+}
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
+  w.Header().Set("Content-Type", "application/json")
+  db, err = gorm.Open("sqlite3", "test_database.db")
+  if err != nil {
+    fmt.Println(err.Error())
+    panic("Failed to connect to database")
+  }
+  defer db.Close()
+
+  var mybooks []Book
+  db.Find(&mybooks)
+  json.NewEncoder(w).Encode(mybooks)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, book := range books {
-		if book.ID == params["id"] {
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Book{})
+  w.Header().Set("Content-Type", "application/json")
+  db, err = gorm.Open("sqlite3", "test_database.db")
+  if err != nil {
+    fmt.Println(err.Error())
+    panic("Failed to connect to database")
+  }
+  defer db.Close()
+  params := mux.Vars(r)
+  var book Book
+  db.Where("id = ?", params["id"]).Find(&book)
+  json.NewEncoder(w).Encode(book)
 }
 
 func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(1000000))
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
+  w.Header().Set("Content-Type", "application/json")
+  db, err = gorm.Open("sqlite3", "test_database.db")
+  if err != nil {
+    fmt.Println(err.Error())
+    panic("Failed to connect to database")
+  }
+  defer db.Close()
+
+    var book Book
+  _ = json.NewDecoder(r.Body).Decode(&book)
+  db.Create(&book)
+  json.NewEncoder(w).Encode(book)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, book := range books {
-		if book.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(books)
+  w.Header().Set("Content-Type", "application/json")
+  db, err = gorm.Open("sqlite3", "test_database.db")
+  if err != nil {
+    fmt.Println(err.Error())
+    panic("Failed to connect to database")
+  }
+  defer db.Close()
+  params := mux.Vars(r)
+  var book Book
+  var bodyData Book
+  _ = json.NewDecoder(r.Body).Decode(&bodyData)
+  db.Where("id = ?", params["id"]).Find(&book)
+  book.Isbn = bodyData.Isbn
+  book.Title = bodyData.Title
+  db.Save(&book)
+  json.NewEncoder(w).Encode(updateBook)
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, book := range books {
-		if book.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
+  db, err = gorm.Open("sqlite3", "test_database.db")
+  if err != nil {
+    fmt.Println(err.Error())
+    panic("Failed to connect to database")
+  }
+  defer db.Close()
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  var book Book
+  db.Where("id = ?", params["id"]).Find(&book)
+  db.Delete(&book)
+  fmt.Fprintf(w, "User deleted")
+}
+
+func handleRequests() {
+  r := mux.NewRouter().StrictSlash(true)
+
+  r.HandleFunc("/api/books", getBooks).Methods("GET")
+  r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+  r.HandleFunc("/api/books", createBook).Methods("POST")
+  r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+  r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+  log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func main() {
-	r := mux.NewRouter()
-
-	// Mock Data - @todo - implement DB
-	books = append(books, Book{ID: "1", Isbn: "4525262", Title: "Book one", Author: &Author{Firstname: "Ali", Lastname: "Sol"}})
-	books = append(books, Book{ID: "2", Isbn: "44454262", Title: "Book two", Author: &Author{Firstname: "Ali2", Lastname: "Sol2"}})
-
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
-
-	log.Fatal(http.ListenAndServe(":8000", r))
+  initialMigration()
+  handleRequests()
 }
